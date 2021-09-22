d076963f3c0d426cf4818447ca79fec8597a2fed
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-22 22:49:49 +0000

    setting up wasd movement (input.js) and exporting to play.js

--- input.js
+++ input.js
@@ -1,5 +1,33 @@
-// const net = require("net");
+let connection;
+//listens to the input
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  //translates to eng from comp lang ..specifies which language to
+  stdin.setEncoding("utf8");
+  stdin.resume();
 
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up")
+  }
+  if (key === "a") {
+    connection.write("Move: left")
+  }
+  if (key === "s") {
+    connection.write("Move: down")
+  }
+  if (key === "d") {
+    connection.write("Move: right")
+  }
+}
 
-// module.exports = setupInput;
+module.exports = { setupInput };
