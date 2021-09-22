dff6eda0909bbd701a266115b40fce0ca5d4834a
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-09-22 18:51:52 +0000

    creating a separate module(client.js) for the TCP Connection

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    // IP address here, as a string
-    port: 50542, 
-    // PORT number here,
-  });
-  const name = 'KAN';
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //below adjusts the connect function to handle incoming data
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
